0da6aba13adfc8865367c14e7243e97aede701d5
CaPartDansLeDecor <mathis.guilhin@insa-lyon.fr>
2019-03-29 17:55:38 +0100

    wip

---
+++ fichiers/ajax/infosPaysMouse.xsl
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<!-- New document created with EditiX at Fri Mar 22 16:57:14 CET 2019 -->
+
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+	<xsl:output method="html"/>
+	<xsl:param name="nomPays"/>
+	<xsl:template match="/">
+	<html>
+		<body>
+            <div>
+                <xsl:value-of select="//country[name/common=$nomPays]/capital/text()"/>
+            </div>
+            <div>
+                <xsl:variable name="code" select="//country[name/common=$nomPays]/codes/cca2/text()"/>
+                <xsl:variable name="lowercase" select="'abcdefghijklmnopqrstuvwxyz'" />
+                <xsl:variable name="uppercase" select="'ABCDEFGHIJKLMNOPQRSTUVWXYZ'" />
+                <img src="{concat('http://www.geonames.org/flags/x/',translate($code, $uppercase, $lowercase),'.gif')}" alt="" height="40" width="60"/>
+                
+            </div>
+		</body>
+	</html>
+	</xsl:template>
+
+</xsl:stylesheet>
+
+
--- fichiers/ajax/partieJavascript.js
+++ fichiers/ajax/partieJavascript.js
@@ -8,6 +8,8 @@ function recupererPremierEnfantDeTypeNode(n) {
     return x;
 }
 
+
+
 function button1() {
     document.body.style.background = 'blue';
     document.getElementById("myButton1").style.color = "white";
@@ -26,38 +28,130 @@ function button3(xmlDocumentUrl, xslDocumentUrl) {
     xsltProcessor.importStylesheet(xslDocument);
     var newXmlDocument = xsltProcessor.transformToDocument(xmlDocument);
     
-    // Recherche du parent (dont l'id est "here") de l'élément à remplacer dans le document HTML courant
+    // Recherche du parent (dont l'id est "here") de l'?l?ment ? remplacer dans le document HTML courant
     var elementHtmlParent = window.document.getElementById("id_element_a_remplacer");
-    // Premier élément fils du parent
+    // Premier ?l?ment fils du parent
     var elementHtmlARemplacer = recupererPremierEnfantDeTypeNode(elementHtmlParent);
-    // Premier élément "elementName" du nouveau document (par exemple, "ul", "table"...)
+    // Premier ?l?ment "elementName" du nouveau document (par exemple, "ul", "table"...)
     var elementAInserer = newXmlDocument.getElementsByTagName("h3")[0];
     
-    // Remplacement de l'élément
+    // Remplacement de l'?l?ment
     elementHtmlParent.replaceChild(elementAInserer, elementHtmlARemplacer);
 }
 
 function bouton4_displaySvg(){
-	// Recherche du parent (dont l'id est "here") de l'élément à remplacer dans le document HTML courant
-    var elementHtmlParent = window.document.getElementById("id_element_a_remplacer");
-    // Premier élément fils du parent
-    var elementHtmlARemplacer = recupererPremierEnfantDeTypeNode(elementHtmlParent);
-    // Premier élément "elementName" du nouveau document (par exemple, "ul", "table"...)
-    var elementAInserer = document.getElementById('svg-object');
+  
+    var xsltProcessor = new XSLTProcessor();
+    var xmlDocument = chargerHttpXML('exemple.svg');
     
-    // Remplacement de l'élément
-    elementHtmlParent.replaceChild(elementAInserer, elementHtmlARemplacer);
+	// Recherche du parent (dont l'id est "here") de l'?l?ment ? remplacer dans le document HTML courant
+    var elementHtmlParent = window.document.getElementById("id_element_a_remplacer");
+    // Premier ?l?ment fils du parent
+    var elementHtmlARemplacer = recupererPremierEnfantDeTypeNode(elementHtmlParent);
+    // Premier ?l?ment "elementName" du nouveau document (par exemple, "ul", "table"...)
+    
+    var texteAInserer = new XMLSerializer().serializeToString(xmlDocument);
+    
+    // Remplacement de l'?l?ment
+    elementHtmlARemplacer.innerHTML=texteAInserer;
 
 }
+
+function bouton5_clickable(){
+
+    var shapes = document.getElementsByTagName("g")[0].childNodes;
+    for(var i = 0 ; i<shapes.length ; i++){
+      if(shapes[i].nodeType == 1){
+        shapes[i].addEventListener("click",function(){
+          setNom(this.getAttribute('title'));
+        })
+      }
+    }
+}
+
+function bouton6_displayWorldSvg(){
+  
+    var xsltProcessor = new XSLTProcessor();
+    var xmlDocument = chargerHttpXML('worldHigh.svg');
+    
+	// Recherche du parent (dont l'id est "here") de l'?l?ment ? remplacer dans le document HTML courant
+    var elementHtmlParent = window.document.getElementById("id_element_a_remplacer");
+    // Premier ?l?ment fils du parent
+    var elementHtmlARemplacer = recupererPremierEnfantDeTypeNode(elementHtmlParent);
+    // Premier ?l?ment "elementName" du nouveau document (par exemple, "ul", "table"...)
+    
+    var texteAInserer = new XMLSerializer().serializeToString(xmlDocument);
+    
+    // Remplacement de l'?l?ment
+    elementHtmlARemplacer.innerHTML=texteAInserer;
+
+}
+
+function bouton7_clickable(){
+  var countries = document.getElementsByTagName("g")[0].childNodes;
+  for(var i = 0 ; i<countries.length ; i++){
+        countries[i].addEventListener("click",function(){
+          setNom(this.getAttribute('countryname'));
+        })
+    }
+}
+
+function bouton8_mouse(){
+    
+    var xsltProcessor = new XSLTProcessor();
+    var xslDocument = chargerHttpXML('infosPaysMouse.xsl');
+    var xmlDocument = chargerHttpXML('countriesTP.xml');
+    xsltProcessor.importStylesheet(xslDocument);
+
+  var countries = document.getElementsByTagName("g")[0].childNodes;
+  var tableau = document.getElementById("tableau");
+  for(var i = 0 ; i<countries.length ; i++){
+        countries[i].addEventListener("mouseover",function(){
+          tableau.style.display = "";
+          xsltProcessor.setParameter(null, "nomPays", this.getAttribute('countryname'));
+          
+          var newXmlDocument = xsltProcessor.transformToDocument(xmlDocument);
+          setTableau(this.getAttribute('countryname'), newXmlDocument.getElementsByTagName("div")[0], newXmlDocument.getElementsByTagName("div")[1]);
+          this.setAttribute("style", "fill:red");
+          
+        });
+        countries[i].addEventListener("mouseleave",function(){
+          tableau.style.display = "none";
+          setTableau("","","");
+          this.setAttribute("style", "");
+        });
+    }
+}
+
+function bouton9(){
+    var xmlDocument = chargerHttpXML('countriesTP.xml');
+    var pays = xmlDocument.getElementsByTagName("common");
+    for(var i = 0 ; i<pays.length ; i++){
+      var node = document.createElement("option",{ value : pays[i].nodeValue });
+      window.document.getElementById("pays").appendChild(node);
+    }
+}
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//change le contenu de l'élement avec l'id "nom" avec la chaine de caractéres en paramètre	  
+//change le contenu de l'?lement avec l'id "nom" avec la chaine de caract?res en param?tre	  
 function setNom(nom) {
     var elementHtmlARemplir = window.document.getElementById("id_nom_a_remplacer");
     elementHtmlARemplir.innerHTML = nom;
 }
 
+function setTableau(nomPays, capitale, drapeau) {
+    var elementHtmlARemplir1 = window.document.getElementById("nomPays");
+    elementHtmlARemplir1.innerHTML = nomPays;
+    window.document.getElementById("capitale").innerHTML = capitale.innerHTML;
+    
+    
+                    
+    window.document.getElementById("drapeau").innerHTML = drapeau.innerHTML;
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//charge le fichier XML se trouvant à l'URL relative donné dans le paramètreet le retourne
+//charge le fichier XML se trouvant ? l'URL relative donn? dans le param?treet le retourne
 function chargerHttpXML(xmlDocumentUrl) {
 
     var httpAjax;
@@ -70,7 +164,7 @@ function chargerHttpXML(xmlDocumentUrl) {
         httpAjax.overrideMimeType('text/xml');
     }
 
-    //chargement du fichier XML à l'aide de XMLHttpRequest synchrone (le 3° paramètre est défini à false)
+    //chargement du fichier XML ? l'aide de XMLHttpRequest synchrone (le 3? param?tre est d?fini ? false)
     httpAjax.open('GET', xmlDocumentUrl, false);
     httpAjax.send();
 
@@ -78,7 +172,7 @@ function chargerHttpXML(xmlDocumentUrl) {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////  
-// Charge le fichier JSON se trouvant à l'URL donnée en paramètre et le retourne
+// Charge le fichier JSON se trouvant ? l'URL donn?e en param?tre et le retourne
 function chargerHttpJSON(jsonDocumentUrl) {
 
     var httpAjax;
@@ -91,7 +185,7 @@ function chargerHttpJSON(jsonDocumentUrl) {
         httpAjax.overrideMimeType('text/xml');
     }
 
-    // chargement du fichier JSON à l'aide de XMLHttpRequest synchrone (le 3° paramètre est défini à false)
+    // chargement du fichier JSON ? l'aide de XMLHttpRequest synchrone (le 3? param?tre est d?fini ? false)
     httpAjax.open('GET', jsonDocumentUrl, false);
     httpAjax.send();
 
@@ -105,26 +199,26 @@ function Bouton2_ajaxBibliographie(xmlDocumentUrl, xslDocumentUrl, newElementNam
 
     var xsltProcessor = new XSLTProcessor();
 
-    // Chargement du fichier XSL à l'aide de XMLHttpRequest synchrone 
+    // Chargement du fichier XSL ? l'aide de XMLHttpRequest synchrone 
     var xslDocument = chargerHttpXML(xslDocumentUrl);
 
     // Importation du .xsl
     xsltProcessor.importStylesheet(xslDocument);
 
-    // Chargement du fichier XML à l'aide de XMLHttpRequest synchrone 
+    // Chargement du fichier XML ? l'aide de XMLHttpRequest synchrone 
     var xmlDocument = chargerHttpXML(xmlDocumentUrl);
 
-    // Création du document XML transformé par le XSL
+    // Cr?ation du document XML transform? par le XSL
     var newXmlDocument = xsltProcessor.transformToDocument(xmlDocument);
 
-    // Recherche du parent (dont l'id est "here") de l'élément à remplacer dans le document HTML courant
+    // Recherche du parent (dont l'id est "here") de l'?l?ment ? remplacer dans le document HTML courant
     var elementHtmlParent = window.document.getElementById("id_element_a_remplacer");
-    // Premier élément fils du parent
+    // Premier ?l?ment fils du parent
     var elementHtmlARemplacer = recupererPremierEnfantDeTypeNode(elementHtmlParent);
-    // Premier élément "elementName" du nouveau document (par exemple, "ul", "table"...)
+    // Premier ?l?ment "elementName" du nouveau document (par exemple, "ul", "table"...)
     var elementAInserer = newXmlDocument.getElementsByTagName(newElementName)[0];
 
-    // Remplacement de l'élément
+    // Remplacement de l'?l?ment
     elementHtmlParent.replaceChild(elementAInserer, elementHtmlARemplacer);
 
 }
@@ -135,13 +229,13 @@ function Bouton3_ajaxEmployees(xmlDocumentUrl) {
 
     var xmlDocument = chargerHttpXML(xmlDocumentUrl);
 
-    //extraction des noms à partir du document XML (avec une feuille de style ou en javascript)
+    //extraction des noms ? partir du document XML (avec une feuille de style ou en javascript)
     var lesNoms = xmlDocument.getElementsByTagName("LastName");
 
     // Parcours de la liste des noms avec une boucle for et 
-    // construction d'une chaine de charactères contenant les noms séparés par des espaces 
+    // construction d'une chaine de charact?res contenant les noms s?par?s par des espaces 
     // Pour avoir la longueur d'une liste : attribut 'length'
-    // Accès au texte d'un noeud "LastName" : NOM_NOEUD.firstChild.nodeValue
+    // Acc?s au texte d'un noeud "LastName" : NOM_NOEUD.firstChild.nodeValue
     var chaineDesNoms = "";
     for (i = 0; i < lesNoms.length; i++) {
         if (i > 0) {
@@ -151,7 +245,7 @@ function Bouton3_ajaxEmployees(xmlDocumentUrl) {
     }
 
 
-    // Appel (ou recopie) de la fonction setNom(...) ou bien autre façon de modifier le texte de l'élément "span"
+    // Appel (ou recopie) de la fonction setNom(...) ou bien autre fa?on de modifier le texte de l'?l?ment "span"
     setNom(chaineDesNoms);
 
 
@@ -161,6 +255,6 @@ function Bouton3_ajaxEmployees(xmlDocumentUrl) {
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function Bouton4_ajaxEmployeesTableau(xmlDocumentUrl, xslDocumentUrl) {
-    //commenter la ligne suivante qui affiche la boîte de dialogue!
-    alert("Fonction à compléter...");
+    //commenter la ligne suivante qui affiche la bo?te de dialogue!
+    alert("Fonction ? compl?ter...");
 }
